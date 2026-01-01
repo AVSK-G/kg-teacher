c5c220d54314f3cef9e1d5adb272578d2bcb1e48
AVSK-G <avishekpaal@gmail.com>
2026-01-01 06:44:47 +0530

    sw.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,39 @@
-const CACHE_NAME = "kg-teacher-v2";
+const CACHE_NAME = "kg-teacher-v3";
 
 const FILES_TO_CACHE = [
-  "./",
   "./index.html",
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png"
 ];
 
-self.addEventListener("install", e => {
-  self.skipWaiting();
-  e.waitUntil(
+self.addEventListener("install", event => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("activate", e => {
-  e.waitUntil(
+self.addEventListener("activate", event => {
+  event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
-        keys.map(k => {
-          if (k !== CACHE_NAME) return caches.delete(k);
-        })
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
       )
     )
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
-});
+self.addEventListener("fetch", event => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match("./index.html"))
+    );
+    return;
+  }
 
-self.addEventListener("message", e => {
-  if (e.data === "SKIP_WAITING") self.skipWaiting();
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
 });
